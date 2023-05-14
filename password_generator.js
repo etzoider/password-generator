a3f52d4505afdfc141dc41d95fba6f359fb4e138
Rafael <98551688+etzoider@users.noreply.github.com>
2023-05-14 12:11:46 -0300

    Changing generator script

--- password_generator.js
+++ password_generator.js
@@ -1,13 +1,47 @@
+const MAX_PASSWORD_LENGTH = 10;
 
-const checkboxes = document.q
+function scrambleString(string) {
+    let chars = string.split('');
+    
+    for (let i = chars.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = chars[i];
+      chars[i] = chars[j];
+      chars[j] = temp;
+    }
+    
+    let result = chars.join('');
+    
+    return result;
+}
+
+function uglifyString(string) {
+    let result = '';
+    
+    for (let i = 0; i < string.length; i++) {
+      let char = string.charAt(i);
+      
+      // Randomly determine whether to convert the character to uppercase or lowercase
+      if (Math.random() < 0.5) {
+        char = char.toUpperCase();
+      } else {
+        char = char.toLowerCase();
+      }
+      
+      result += char;
+    }
+    
+    return result;
+}
+
+const chooseRandomNumber = (maxRange) => { //range is non-inclusive. (10) will return from 0 to 9 because math.floor
+    return Math.floor(Math.random() * (range))
+}
 
 
-
-
-
-function password_generator(char_amount) {
-    if (typeof char_amount != 'number') {
-        return (`"${char_amount}" is not a valid amount. Please, insert a numeric value.`)
+function password_generator(availableChars, charAmount) {
+    if (typeof charAmount != 'number') {
+        return (`"${charAmount}" is not a valid amount. Please, insert a numeric value.`)
     } 
     
     //troquei o IF else por só um return no IF(a validacao no começo da função serve pra impedir que 
@@ -15,40 +49,50 @@ function password_generator(char_amount) {
     //é desnecessário o else)
 
 
-    let new_password = ""
-    const char_types = {
-        special_chars: ['!', '@', '#', '$', '%', '&', '*'],
-        letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-        numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let password = []
+
+    while (password.length < charAmount) {
+        const randomIndex = chooseRandomNumber
     }
+   
 
-    const generates_random_num = range => {
-        return Math.floor(Math.random() * (range))
-    }
-
-
-    while (new_password.length < char_amount) {
-        const current_char_type = () => {
-            switch (generates_random_num(7)) {
-                case 1: case 2:
-                    return 'special_chars'
-                case 3: case 4: case 5:
-                    return 'letters'
-                case 6: case 7:
-                    return 'numbers'
-            }
-        }
-        
-        const current_char_index = generates_random_num(char_types[current_char_type()].length - 1)
-        const current_char = char_types[current_char_type()][current_char_index]
-
-        new_password = new_password.concat(current_char)
-    }
     return new_password
 }
 
 
 
-function generatePassword() {
-    alert('hi');
+const checkboxes = document.querySelector('.checkbox-wrapper').querySelectorAll('div');
+const submitBtn = document.querySelector(".submit-btn");
+
+submitBtn.addEventListener('click', () => handleSubmit());
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', (e) => handleCheckboxClick(e))
+})
+
+const handleCheckboxClick = (e) => {
+    e.stopPropagation();
+    const clickedBox = e.target;
+    const isChecked = clickedBox.classList.contains('checked');
+    
+    (isChecked) ? clickedBox.classList.remove('checked') : clickedBox.classList.add('checked');
+    
+}
+
+const handleSubmit = () => {
+    const availableChars = [];
+    const charTypes = {
+      'symbols':  ['!', '@', '#', '$', '%', '&', '*'],
+        'letters': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+       'numbers': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    };
+
+
+    [...checkboxes].forEach( box => {
+        if (!box.classList.contains('checked')) return
+        
+        availableChars.push(...charTypes[box.id]);
+
+    })
+
+    password_generator(availableChars, chooseRandomNumber(MAX_PASSWORD_LENGTH));
 }
